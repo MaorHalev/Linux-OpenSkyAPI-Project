9128a2df4a17343f9ea73062ea52435a922142a7
MaorHalev <maorhalevi@gmail.com>
2023-03-24 14:51:55 +0300

    if u recruit - take me

--- testscript.sh
+++ testscript.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+now=$(date +%s)
+yesterday=$(date --date="yesterday" +"%s")
+curl -u "Halevi:0121596" -s "https://opensky-network.org/api/flights/departure?airport=$1&begin=$yesterday&end=$now" > dpts
+sed -i 's/[{}]/\n/g' dpts   #delete {}
+sed -i 's/\[//g' dpts       #delete [
+sed -i 's/\]//g' dpts       #delete ]
+sed -i 's/^[ \t,]*//' dpts  #delete line starts with , \t
+awk -F',;' '{ for (i=2;i<=NF;i+=2) print $i }' dpts
 
-echo blabla
+sed -i '/^$/d' dpts         #delete empty lines
