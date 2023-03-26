1bf53f59eaa48352ba6615aebf21897977b59da2
MaorHalev <maorhalevi@gmail.com>
2023-03-26 20:16:53 +0300

    pipelined sed arguments

--- lastFlightsScript.sh
+++ lastFlightsScript.sh
@@ -10,25 +10,9 @@ for airport in $PARAMS
 do
 mkdir "$airport"
 cd "$airport"
-curl -u "Halevi:0121596" -s "https://opensky-network.org/api/flights/departure?airport=$airport&begin=$yesterday&end=$now" > dptsTmp
-sed -i 's/,/\n/g' dptsTmp 
-awk -F: '{print $2 }' dptsTmp > "$airport.dpt" 
-sed -i -z 's/\n/,/g' "$airport.dpt" 
-sed -i 's/}/\n/g' "$airport.dpt" 
-sed -i 's/"//g' "$airport.dpt" 
-sed -i 's/[ \t]//' "$airport.dpt"
-sed -i '$ d' "$airport.dpt" 
-sed -i 's/^[,]//g' "$airport.dpt" 
-rm dptsTmp
-curl -u "Halevi:0121596" -s "https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$yesterday&end=$now" > arvTmp
-sed -i 's/,/\n/g' arvTmp 
-awk -F: '{print $2 }' arvTmp > "$airport.arv" 
-sed -i -z 's/\n/,/g' "$airport.arv" 
-sed -i 's/}/\n/g' "$airport.arv" 
-sed -i 's/"//g' "$airport.arv" 
-sed -i 's/[ \t]//' "$airport.arv" 
-sed -i '$ d' "$airport.arv" 
-sed -i 's/^[,]//g' "$airport.arv" 
-rm arvTmp
+curl -u "Halevi:0121596" -s "https://opensky-network.org/api/flights/departure?airport=$airport&begin=$yesterday&end=$now" | sed 's/,/\n/g' | awk -F: '{print $2 }' | sed -z 's/\n/,/g' | sed 's/}/\n/g' | sed 's/[][ \t"]//g' | sed 's/^[,]//g' > "$airport.dpt"
+curl -u "Halevi:0121596" -s "https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$yesterday&end=$now" | sed 's/,/\n/g' | awk -F: '{print $2 }' | sed -z 's/\n/,/g' | sed 's/}/\n/g' | sed 's/[][ \t"]//g' | sed 's/^[,]//g'  > "$airport.arv"
+echo "icao24,firstSeen,estDepartueAirport,lastSeen,estArrivalAirport,callsign,estDepartueAirportHorizDistance,estDepartureAirportVertDistance,estArrivalAirportHorizDistance,estArrivalAirportVertDistance,departueAirportCandidatesCount,arrivalAirportCandidatesCount" | cat - "$airport.dpt" > tmp && mv tmp "$airport.dpt"
+echo "icao24,firstSeen,estDepartueAirport,lastSeen,estArrivalAirport,callsign,estDepartueAirportHorizDistance,estDepartureAirportVertDistance,estArrivalAirportHorizDistance,estArrivalAirportVertDistance,departueAirportCandidatesCount,arrivalAirportCandidatesCount" | cat - "$airport.arv" > tmp && mv tmp "$airport.arv"
 cd ..
 done
