6fafbd5e3b417c2d80ec235e85d282f35a123dcd
MaorHalev <maorhalevi@gmail.com>
2023-04-12 12:59:11 +0300

    updated bash

--- lastFlightsScript.sh
+++ lastFlightsScript.sh
@@ -6,13 +6,20 @@ cd flightsDB
 now=$(date +%s)
 yesterday=$(date --date="yesterday" +"%s")
 PARAMS="$@"
-for airport in $PARAMS
+for airport in $PARAMS;
 do
-mkdir "$airport"
+dptUrl="https://opensky-network.org/api/flights/departure?airport=$airport&begin=$yesterday&end=$now";
+arvUrl="https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$yesterday&end=$now";
+response=$(curl -s -o /dev/null -w "%{http_code}" $dptUrl)
+if [ $response == "200" ]; then
+    mkdir "$airport"
 cd "$airport"
-curl -u "Halevi:0121596" -s "https://opensky-network.org/api/flights/departure?airport=$airport&begin=$yesterday&end=$now" | sed 's/,/\n/g' | awk -F: '{print $2 }' | sed -z 's/\n/,/g' | sed 's/}/\n/g' | sed 's/[][ \t"]//g' | sed 's/^[,]//g' > "$airport.dpt"
-curl -u "Halevi:0121596" -s "https://opensky-network.org/api/flights/arrival?airport=$airport&begin=$yesterday&end=$now" | sed 's/,/\n/g' | awk -F: '{print $2 }' | sed -z 's/\n/,/g' | sed 's/}/\n/g' | sed 's/[][ \t"]//g' | sed 's/^[,]//g'  > "$airport.arv"
+curl -u "Halevi:0121596" -s $dptUrl | sed 's/,/\n/g' | awk -F: '{print $2 }' | sed -z 's/\n/,/g' | sed 's/}/\n/g' | sed 's/[][ \t"]//g' | sed 's/^[,]//g' > "$airport.dpt"
+curl -u "Halevi:0121596" -s $arvUrl | sed 's/,/\n/g' | awk -F: '{print $2 }' | sed -z 's/\n/,/g' | sed 's/}/\n/g' | sed 's/[][ \t"]//g' | sed 's/^[,]//g'  > "$airport.arv"
 echo "icao24,firstSeen,estDepartueAirport,lastSeen,estArrivalAirport,callsign,estDepartueAirportHorizDistance,estDepartureAirportVertDistance,estArrivalAirportHorizDistance,estArrivalAirportVertDistance,departueAirportCandidatesCount,arrivalAirportCandidatesCount" | cat - "$airport.dpt" > tmp && mv tmp "$airport.dpt"
 echo "icao24,firstSeen,estDepartueAirport,lastSeen,estArrivalAirport,callsign,estDepartueAirportHorizDistance,estDepartureAirportVertDistance,estArrivalAirportHorizDistance,estArrivalAirportVertDistance,departueAirportCandidatesCount,arrivalAirportCandidatesCount" | cat - "$airport.arv" > tmp && mv tmp "$airport.arv"
 cd ..
+else
+    echo "ICAO $airport does not exist";
+fi
 done
